5c6413ea6802e51bed130f9ebf654c865be243af
zhou <304863859@qq.com>
2013-08-20 14:56:32 +0800

    112

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php
 $classExample = array();
+$k=1;
 $soap = new SoapServer(null,array('uri'=>"http://116.255.243.142/",'classExample'=>$classExample));
 $soap->setClass('chesterClass');
 $soap->handle();
